628743ae97dc5feee0517c50a6409b20c3f02755
Syed Shamsuddin <007shamsuddin@gmail.com>
2018-08-31 14:42:51 +0530

    new DB_URI
    DataBase Connection changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var configure = require("./Server/configure");
 var Mongoose = require("mongoose");
-var uri = "mongodb://Emir28107:MuskanEmir143@ds237072.mlab.com:37072/imcha";
+var uri = process.env.MONGODB_URI;
 
 if(process.env.NODE_ENV !== "production") {
     require("dotenv").config();
